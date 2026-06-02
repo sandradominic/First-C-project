7439fcba99c08107ecab6562f6ef4115a6da344c
sandradominic <sandra.dominic.184@gmail.com>
2026-06-02 21:45:48 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    float num1, num2, result;
+
+    // Input
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    // Switch case
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f", result);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result = %.2f", result);
+            } else {
+                printf("Error! Division by
